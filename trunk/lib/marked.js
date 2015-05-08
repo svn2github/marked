37322d8bfdc75c7ef580ac06893c41f253fbd7dc
christopher.jeffrey..jj <christopher.jeffrey..jj.@de94abd1-4c94-9ff3-2ac9-ce57b7db7bff>
2015-05-08 01:31:05 +0000

    Merge pull request #468 from trinketapp/master
    Ability to provide a custom sanitize routine
    
    git-svn-id: https://github.com/chjj/marked.git@660 de94abd1-4c94-9ff3-2ac9-ce57b7db7bff

--- trunk/lib/marked.js
+++ trunk/lib/marked.js
@@ -75,7 +75,7 @@ block.normal = merge({}, block);
  */
 
 block.gfm = merge({}, block.normal, {
-  fences: /^ *(`{3,}|~{3,}) *(\S+)? *\n([\s\S]+?)\s*\1 *(?:\n+|$)/,
+  fences: /^ *(`{3,}|~{3,})[ \.]*(\S+)? *\n([\s\S]+?)\s*\1 *(?:\n+|$)/,
   paragraph: /^/
 });
 
@@ -680,7 +680,7 @@ InlineLexer.prototype.output = function(src) {
     // text
     if (cap = this.rules.text.exec(src)) {
       src = src.substring(cap[0].length);
-      out += escape(this.smartypants(cap[0]));
+      out += this.renderer.text(escape(this.smartypants(cap[0])));
       continue;
     }
 
@@ -713,8 +713,10 @@ InlineLexer.prototype.outputLink = function(cap, link) {
 InlineLexer.prototype.smartypants = function(text) {
   if (!this.options.smartypants) return text;
   return text
-    // em-dashes
-    .replace(/--/g, '\u2014')
+    // em-dashes    
+    .replace(/---/g, '\u2014')
+    // en-dashes
+    .replace(/--/g, '\u2013')
     // opening singles
     .replace(/(^|[-\u2014/(\[{"\s])'/g, '$1\u2018')
     // closing singles & apostrophes
@@ -732,6 +734,7 @@ InlineLexer.prototype.smartypants = function(text) {
  */
 
 InlineLexer.prototype.mangle = function(text) {
+  if (!this.options.mangle) return text;
   var out = ''
     , l = text.length
     , i = 0
@@ -870,7 +873,7 @@ Renderer.prototype.link = function(href, title, text) {
     } catch (e) {
       return '';
     }
-    if (prot.indexOf('javascript:') === 0) {
+    if (prot.indexOf('javascript:') === 0 || prot.indexOf('vbscript:') === 0) {
       return '';
     }
   }
@@ -889,6 +892,10 @@ Renderer.prototype.image = function(href, title, text) {
   }
   out += this.options.xhtml ? '/>' : '>';
   return out;
+};
+
+Renderer.prototype.text = function(text) {
+  return text;
 };
 
 /**
@@ -1234,6 +1241,7 @@ marked.defaults = {
   breaks: false,
   pedantic: false,
   sanitize: false,
+  mangle: true,
   smartLists: false,
   silent: false,
   highlight: null,
