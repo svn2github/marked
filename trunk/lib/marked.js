52c619ef8a50b185b090d66e6e1d0c0d1a64b5aa
christopher.jeffrey..jj <christopher.jeffrey..jj.@de94abd1-4c94-9ff3-2ac9-ce57b7db7bff>
2015-05-08 01:21:23 +0000

    Merge pull request #542 from dchest/nomangle
    Add -mangle/-no-mangle to enable/disable mangling of email addresses.
    
    git-svn-id: https://github.com/chjj/marked.git@653 de94abd1-4c94-9ff3-2ac9-ce57b7db7bff

--- trunk/lib/marked.js
+++ trunk/lib/marked.js
@@ -75,7 +75,7 @@ block.normal = merge({}, block);
  */
 
 block.gfm = merge({}, block.normal, {
-  fences: /^ *(`{3,}|~{3,}) *(\S+)? *\n([\s\S]+?)\s*\1 *(?:\n+|$)/,
+  fences: /^ *(`{3,}|~{3,})[ \.]*(\S+)? *\n([\s\S]+?)\s*\1 *(?:\n+|$)/,
   paragraph: /^/
 });
 
@@ -678,7 +678,7 @@ InlineLexer.prototype.output = function(src) {
     // text
     if (cap = this.rules.text.exec(src)) {
       src = src.substring(cap[0].length);
-      out += escape(this.smartypants(cap[0]));
+      out += this.renderer.text(escape(this.smartypants(cap[0])));
       continue;
     }
 
@@ -888,6 +888,10 @@ Renderer.prototype.image = function(href, title, text) {
   }
   out += this.options.xhtml ? '/>' : '>';
   return out;
+};
+
+Renderer.prototype.text = function(text) {
+  return text;
 };
 
 /**
