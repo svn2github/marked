4cafd02fa48fc5c434fe754b944b3c86480f3415
vse.mozhet.byt <vse.mozhet.byt@de94abd1-4c94-9ff3-2ac9-ce57b7db7bff>
2018-03-27 12:44:02 +0000

    Update link to demo page in README.md
    git-svn-id: https://github.com/chjj/marked.git@1174 de94abd1-4c94-9ff3-2ac9-ce57b7db7bff

--- branches/release-0.3.19/test/unit/marked-spec.js
+++
@@ -1,7 +0,0 @@
-var marked = require('../../lib/marked.js');
-
-it('should run the test', function () {
-  spyOn(marked, 'parse').and.callThrough();
-  marked.parse('Hello World!');
-  expect(marked.parse).toHaveBeenCalled();
-});
