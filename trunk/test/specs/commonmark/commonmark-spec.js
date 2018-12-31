ef637e294cb020feca7c54eeda7b4d7841af3751
josh.bruce <josh.bruce@de94abd1-4c94-9ff3-2ac9-ce57b7db7bff>
2018-12-31 22:51:42 +0000

    Merge pull request #1392 from UziTech/defibrillator-mccraveiro
    Add defibrillator badge for mccraveiro
    
    git-svn-id: https://github.com/chjj/marked.git@1744 de94abd1-4c94-9ff3-2ac9-ce57b7db7bff

--- trunk/test/specs/commonmark/commonmark-spec.js
+++ trunk/test/specs/commonmark/commonmark-spec.js
@@ -159,8 +159,7 @@ describe('CommonMark 0.28 Indented code blocks', function() {
 describe('CommonMark 0.28 Fenced code blocks', function() {
   var section = 'Fenced code blocks';
 
-  // var shouldPassButFails = [];
-  var shouldPassButFails = [93, 95, 96, 97, 106, 108, 112];
+  var shouldPassButFails = [];
 
   var willNotBeAttemptedByCoreTeam = [];
 
@@ -234,7 +233,7 @@ describe('CommonMark 0.28 Block quotes', function() {
   var section = 'Block quotes';
 
   // var shouldPassButFails = [];
-  var shouldPassButFails = [198, 199, 200, 201];
+  var shouldPassButFails = [198, 199, 200];
 
   var willNotBeAttemptedByCoreTeam = [];
 
@@ -249,7 +248,7 @@ describe('CommonMark 0.28 List items', function() {
   var section = 'List items';
 
   // var shouldPassButFails = [];
-  var shouldPassButFails = [229, 237, 236, 227, 218, 243, 259, 241, 239, 247, 246, 225, 220, 258, 260, 244];
+  var shouldPassButFails = [237, 236, 227, 218, 243, 259, 241, 239, 247, 225, 220, 258, 260];
 
   var willNotBeAttemptedByCoreTeam = [];
 
@@ -264,7 +263,7 @@ describe('CommonMark 0.28 Lists', function() {
   var section = 'Lists';
 
   // var shouldPassButFails = [];
-  var shouldPassButFails = [282, 270, 280, 278, 273, 274, 264, 265, 276, 279, 267, 269];
+  var shouldPassButFails = [282, 270, 280, 278, 273, 274, 264, 265, 279, 267, 269];
 
   var willNotBeAttemptedByCoreTeam = [];
 
@@ -294,7 +293,7 @@ describe('CommonMark 0.28 Backslash escapes', function() {
   var section = 'Backslash escapes';
 
   // var shouldPassButFails = [];
-  var shouldPassButFails = [291, 300, 301];
+  var shouldPassButFails = [300, 301];
 
   var willNotBeAttemptedByCoreTeam = [];
 
@@ -338,7 +337,7 @@ describe('CommonMark 0.28 Emphasis and strong emphasis', function() {
   var section = 'Emphasis and strong emphasis';
 
   // var shouldPassButFails = [];
-  var shouldPassButFails = [334, 342, 348, 349, 352, 353, 360, 368, 369, 371, 372, 378, 380, 381, 382, 387, 388, 392, 393, 394, 395, 396, 402, 403, 409, 416, 419, 420, 421, 422, 423, 424, 428, 431, 432, 433, 434, 435, 436, 443, 444, 445, 448, 449, 453, 454, 455, 457, 458];
+  var shouldPassButFails = [334, 342, 348, 349, 352, 353, 360, 368, 369, 371, 372, 378, 380, 381, 382, 387, 388, 392, 393, 394, 395, 396, 402, 403, 409, 419, 420, 421, 422, 423, 424, 431, 432, 433, 434, 435, 436, 443, 444, 445, 448, 449, 453, 454, 455, 457, 458];
 
   var willNotBeAttemptedByCoreTeam = [];
 
